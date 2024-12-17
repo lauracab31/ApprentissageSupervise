9c736102b62f28abbf3830e001cdf60802979d07
lauracab31 <147844898+lauracab31@users.noreply.github.com>
2024-12-17 12:19:44 +0100

    Update matrice_equite_sexe.py

--- matrice_equite_sexe.py
+++ matrice_equite_sexe.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 import joblib as jl
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import confusion_matrix
@@ -65,7 +66,7 @@ rf = jl.load('gridSearch_rf.joblib')
 ab = jl.load('gridSearch_ab.joblib')
 gb = jl.load('gridSearch_gb.joblib')
 
-# Entraînement et évaluation des modèles avec la colonne SEX (hommes)
+# Entraînement et évaluation des modèles avec la colonne SEX (hommes)#################################################################################
 rf.fit(X_train_homme, y_train_homme)
 ab.fit(X_train_homme, y_train_homme)
 gb.fit(X_train_homme, y_train_homme)
@@ -74,11 +75,49 @@ y_pred_rf_homme = rf.predict(X_test_homme)
 y_pred_ab_homme = ab.predict(X_test_homme)
 y_pred_gb_homme = gb.predict(X_test_homme)
 
-print("Matrice de confusion Random Forest pour les hommes:\n", confusion_matrix(y_test_homme, y_pred_rf_homme, normalize='all'))
-print("Matrice de confusion AdaBoost pour les hommes:\n", confusion_matrix(y_test_homme, y_pred_ab_homme, normalize='all'))
-print("Matrice de confusion Gradient Boosting pour les hommes:\n", confusion_matrix(y_test_homme, y_pred_gb_homme, normalize='all'))
+#affichage de la matrice de confusion pour prédire la part d'hommes gagnant plus ou moins de 50k annuel avec le modèle RF
+mat_conf_rf_homme = confusion_matrix(y_test_homme, y_pred_rf_homme)
+#print("Matrice de confusion Random Forest pour les hommes:\n", mat_conf_rf_homme)
 
-# Entraînement et évaluation des modèles avec la colonne SEX (femmes)
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_rf_homme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion Random Forest pour les hommes")
+plt.show()
+
+#affichage de la matrice de confusion pour prédire la part d'hommes gagnant plus ou moins de 50k annuel avec le modèle AB
+mat_conf_ab_homme = confusion_matrix(y_test_homme, y_pred_ab_homme)
+#print("Matrice de confusion Adaboost pour les hommes:\n", mat_conf_ab_homme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_ab_homme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion AdaBoost pour les hommes")
+plt.show()
+
+#affichage de la matrice de confusion pour prédire la part d'hommes gagnant plus ou moins de 50k annuel avec le modèle GB
+mat_conf_gb_homme = confusion_matrix(y_test_homme, y_pred_gb_homme)
+#print("Matrice de confusion GradientBoosting pour les hommes:\n", mat_conf_gb_homme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_gb_homme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion GradientBoosting pour les hommes")
+plt.show()
+
+# Entraînement et évaluation des modèles avec la colonne SEX (femmes)###################################################################################
 rf.fit(X_train_femme, y_train_femme)
 ab.fit(X_train_femme, y_train_femme)
 gb.fit(X_train_femme, y_train_femme)
@@ -87,11 +126,49 @@ y_pred_rf_femme = rf.predict(X_test_femme)
 y_pred_ab_femme = ab.predict(X_test_femme)
 y_pred_gb_femme = gb.predict(X_test_femme)
 
-print("Matrice de confusion Random Forest pour les femmes:\n", confusion_matrix(y_test_femme, y_pred_rf_femme, normalize='all'))
-print("Matrice de confusion AdaBoost pour les femmes:\n", confusion_matrix(y_test_femme, y_pred_ab_femme, normalize='all'))
-print("Matrice de confusion Gradient Boosting pour les femmes:\n", confusion_matrix(y_test_femme, y_pred_gb_femme, normalize='all'))
+#affichage de la matrice de confusion pour prédire la part de femmes gagnant plus ou moins de 50k annuel avec le modèle RF
+mat_conf_rf_femme = confusion_matrix(y_test_femme, y_pred_rf_femme)
+#print("Matrice de confusion Random Forest pour les femmes:\n", mat_conf_rf_femme)
 
-# Suppression de la colonne SEX pour tester l'équité des modèles
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_rf_femme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion Random Forest pour les femmes")
+plt.show()
+
+#affichage de la matrice de confusion pour prédire la part de femmes gagnant plus ou moins de 50k annuel avec le modèle AB
+mat_conf_ab_femme = confusion_matrix(y_test_femme, y_pred_ab_femme)
+#print("Matrice de confusion AdaBoost pour les femmes:\n", mat_conf_ab_femme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_ab_femme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion AdaBoost pour les femmes")
+plt.show()
+
+#affichage de la matrice de confusion pour prédire la part de femmes gagnant plus ou moins de 50k annuel avec le modèle GB
+mat_conf_gb_femme = confusion_matrix(y_test_femme, y_pred_gb_femme)
+#print("Matrice de confusion GradientBoosting pour les femmes:\n", mat_conf_gb_femme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_gb_femme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion GradientBoosting pour les femmes")
+plt.show()
+
+# Suppression de la colonne SEX pour tester l'équité des modèles########################################################################################
 train_sans_sex_homme = X_train_homme.drop(columns=['SEX'])
 train_sans_sex_femme = X_train_femme.drop(columns=['SEX'])
 
@@ -101,24 +178,97 @@ test_sans_sex_femme = X_test_femme.drop(columns=['SEX'])
 # Entraînement sans la colonne SEX
 rf.fit(train_sans_sex_homme, y_train_homme)
 y_pred_rf_sansSex_homme = rf.predict(test_sans_sex_homme)
-print("Matrice de confusion Random Forest sans SEX pour les hommes:\n", confusion_matrix(y_test_homme, y_pred_rf_sansSex_homme, normalize='all'))
+
+#affichage de la matrice de confusion pour prédire la part de hommes gagnant plus ou moins de 50k annuel avec le modèle RF
+mat_conf_rf_sansSex_homme = confusion_matrix(y_test_homme, y_pred_rf_sansSex_homme)
+#print("Matrice de confusion RandomForest pour les hommes sans sexe:\n", mat_conf_rf_sansSex_homme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_rf_sansSex_homme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion RandomForest pour les hommes avec élimination de la colonne 'Sexe'")
+plt.show()
 
 rf.fit(train_sans_sex_femme, y_train_femme)
 y_pred_rf_sansSex_femme = rf.predict(test_sans_sex_femme)
-print("Matrice de confusion Random Forest sans SEX pour les femmes:\n", confusion_matrix(y_test_femme, y_pred_rf_sansSex_femme, normalize='all'))
+#affichage de la matrice de confusion pour prédire la part de femmes gagnant plus ou moins de 50k annuel avec le modèle RF
+mat_conf_rf_sansSex_femme = confusion_matrix(y_test_femme, y_pred_rf_sansSex_femme)
+#print("Matrice de confusion RandomForest pour les femmes sans sexe:\n", mat_conf_rf_sansSex_femmes)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_rf_sansSex_femme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion RandomForest pour les femmes avec élimination de la colonne 'Sexe'")
+plt.show()
 
 ab.fit(train_sans_sex_homme, y_train_homme)
 y_pred_ab_sansSex_homme = ab.predict(test_sans_sex_homme)
-print("Matrice de confusion AdaBoost sans SEX pour les hommes:\n", confusion_matrix(y_test_homme, y_pred_ab_sansSex_homme, normalize='all'))
+#affichage de la matrice de confusion pour prédire la part de hommes gagnant plus ou moins de 50k annuel avec le modèle AB
+mat_conf_ab_sansSex_homme = confusion_matrix(y_test_homme, y_pred_ab_sansSex_homme)
+#print("Matrice de confusion AdaBoost pour les hommes sans sexe:\n", mat_conf_ab_sansSex_homme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_ab_sansSex_homme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion AdaBoost pour les hommes avec élimination de la colonne 'Sexe'")
+plt.show()
 
 ab.fit(train_sans_sex_femme, y_train_femme)
 y_pred_ab_sansSex_femme = ab.predict(test_sans_sex_femme)
-print("Matrice de confusion AdaBoost sans SEX pour les femmes:\n", confusion_matrix(y_test_femme, y_pred_ab_sansSex_femme, normalize='all'))
+#affichage de la matrice de confusion pour prédire la part de femmes gagnant plus ou moins de 50k annuel avec le modèle AB
+mat_conf_ab_sansSex_femme = confusion_matrix(y_test_femme, y_pred_ab_sansSex_femme)
+#print("Matrice de confusion AdaBoost pour les femmes sans sexe:\n", mat_conf_ab_sansSex_femme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_ab_sansSex_femme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion AdaBoost pour les femmes avec élimination de la colonne 'Sexe'")
+plt.show()
 
 gb.fit(train_sans_sex_homme, y_train_homme)
 y_pred_gb_sansSex_homme = gb.predict(test_sans_sex_homme)
-print("Matrice de confusion Gradient Boosting sans SEX pour les hommes:\n", confusion_matrix(y_test_homme, y_pred_gb_sansSex_homme, normalize='all'))
+#affichage de la matrice de confusion pour prédire la part de hommes gagnant plus ou moins de 50k annuel avec le modèle GB
+mat_conf_gb_sansSex_homme = confusion_matrix(y_test_homme, y_pred_gb_sansSex_homme)
+#print("Matrice de confusion GradiantBoosting pour les hommes sans sexe:\n", mat_conf_gb_sansSex_homme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_gb_sansSex_homme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion GradientBoosting pour les hommes avec élimination de la colonne 'Sexe'")
+plt.show()
 
 gb.fit(train_sans_sex_femme, y_train_femme)
 y_pred_gb_sansSex_femme = gb.predict(test_sans_sex_femme)
-print("Matrice de confusion Gradient Boosting sans SEX pour les femmes:\n", confusion_matrix(y_test_femme, y_pred_gb_sansSex_femme, normalize='all'))
+#affichage de la matrice de confusion pour prédire la part de femmes gagnant plus ou moins de 50k annuel avec le modèle GB
+mat_conf_gb_sansSex_femme = confusion_matrix(y_test_femme, y_pred_gb_sansSex_femme)
+#print("Matrice de confusion GradientBoosting pour les femmes sans sexe:\n", mat_conf_gb_sansSex_femme)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(mat_conf_gb_sansSex_femme, annot=True, fmt="d", cmap="Blues", cbar=False,
+            xticklabels=["Predicted -50k", "Predicted +50k"],
+            yticklabels=["Actual -50k", "Actual +50k"])
+# Add labels, title, and formatting
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.title("Matrice de confusion GradientBoosting pour les femmes avec élimination de la colonne 'Sexe'")
+plt.show()
